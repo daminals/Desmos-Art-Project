c8ca0b37a0a5141ddfb3bb7c721764a1f29cfd8a
Daniel Kogan <54873722+daminals@users.noreply.github.com>
2020-12-15 14:11:15 -0500

    calc

---
+++ desmos.js
@@ -0,0 +1,34 @@
+// yellow-y
+calculator.setExpression({ 
+  id: '#', 
+  latex: '',
+  color: '#E8AE35'
+});
+
+// orang
+calculator.setExpression({ 
+  id: '#', 
+  latex: '',
+  color: '#F29B38'
+});
+
+// dark orang
+calculator.setExpression({ 
+  id: '#', 
+  latex: '',
+  color: '#DB763E'
+});
+
+// orang but if orang was red
+calculator.setExpression({ 
+  id: '#', 
+  latex: '',
+  color: '#F25738'
+});
+
+// rad (objectively)
+calculator.setExpression({ 
+  id: '#', 
+  latex: '',
+  color: '#F53641'
+});
